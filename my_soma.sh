25b8759d500207a219c1097121343864fb4ab4b3
Levent Oz <leventoz.dev@gmail.com>
2021-10-11 21:48:45 -0700

    Fix my_soma

--- my_soma.sh
+++ my_soma.sh
@@ -1,21 +1,23 @@
-alias pdloz='pushd ~/git/loz'
+MY_GIT_ORG="$HOME~/git/loz"
+
+alias pdmy="pushd $MY_GIT_ORG"
 alias pdsoma='pushd ~/git'
 
-alias pdgater='cd ~/git/loz/sfdc-bazel/projects/services/gater'
-alias pdgater2='cd ~/git/loz/sfdc-bazel/projects/libs/servicelibs/gater/gater'
+alias pdgater="pushd $MY_GIT_ORG/sfdc-bazel/projects/services/gater"
+alias pdgater2="pushd $MY_GIT_ORG/sfdc-bazel/projects/libs/servicelibs/gater/gater"
 
-alias pdgates='pushd ~/git/loz/gate-definitions/gate-definitions/gates'
-alias pdgatest='pushd ~/git/loz/gate-definitions/test-gate-definitions/gates'
+alias pdgates="pushd $MY_GIT_ORG/gate-definitions/gate-definitions/gates"
+alias pdgatest="pushd $MY_GIT_ORG/gate-definitions/test-gate-definitions/gates"
 
-alias pdhtc='pushg ~/git/loz/hightower-client'
-alias pdhts='pushd ~/git/loz/sfdc-bazel/bazel-bin/projects/services/instrumentation/hightower-service'
+alias pdhtc="pushd $MY_GIT_ORG/hightower-client"
+alias pdhts="pushd $MY_GIT_ORG/sfdc-bazel/bazel-bin/projects/services/instrumentation/hightower-service"
 
 # Use different SSH to push ~/git/tok-lp-robot/gate-definitions
 alias tokpush='GIT_SSH_COMMAND="ssh -i ~/.ssh/tok_lp_robot_rsa" git push'
 
 # Kubernetes
-if [[ -f "$HOME/git/loz/hightower-client/kubeutils.sh" ]]; then
+if [[ -f "$MY_GIT_ORG/hightower-client/kubeutils.sh" ]]; then
     # Activating Kube Utils for instrumentation
     export KNAMESPACE=instrumentation
-    source $HOME/git/loz/hightower-client/kubeutils.sh >/dev/null
+    source $MY_GIT_ORG/hightower-client/kubeutils.sh >/dev/null
 fi
