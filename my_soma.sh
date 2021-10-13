d8238079139bf46fd87b7dbf2c6a86777b102ed4
Levent Oz <leventoz.dev@gmail.com>
2021-10-13 14:56:17 -0700

    Add my_soma to my

--- my_soma.sh
+++ my_soma.sh
@@ -1,7 +1,7 @@
-MY_GIT_ORG="$HOME~/git/loz"
+MY_GIT_ORG="$HOME/git/loz"
 
 alias pdmy="pushd $MY_GIT_ORG"
-alias pdsoma='pushd ~/git'
+alias pdsoma="pushd $HOME/git"
 
 alias pdgater="pushd $MY_GIT_ORG/sfdc-bazel/projects/services/gater"
 alias pdgater2="pushd $MY_GIT_ORG/sfdc-bazel/projects/libs/servicelibs/gater/gater"
@@ -16,8 +16,8 @@ alias pdhts="pushd $MY_GIT_ORG/sfdc-bazel/bazel-bin/projects/services/instrument
 alias tokpush='GIT_SSH_COMMAND="ssh -i ~/.ssh/tok_lp_robot_rsa" git push'
 
 # Kubernetes
-if [[ -f "$MY_GIT_ORG/hightower-client/kubeutils.sh" ]]; then
-    # Activating Kube Utils for instrumentation
-    export KNAMESPACE=instrumentation
-    source $MY_GIT_ORG/hightower-client/kubeutils.sh >/dev/null
-fi
+#if [[ -f "$MY_GIT_ORG/hightower-client/kubeutils.sh" ]]; then
+#    # Activating Kube Utils for instrumentation
+#    export KNAMESPACE=instrumentation
+#    source $MY_GIT_ORG/hightower-client/kubeutils.sh >/dev/null
+#fi
