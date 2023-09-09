b85a9cee79d643ef96b733c72c15971402b6f206
Levent Oz <leventoz.dev@gmail.com>
2023-09-08 21:32:34 -0700

    update again

--- my.sh
+++ my.sh
@@ -1,4 +1,30 @@
-declare MY_PATH="$(cd "$(dirname "$0")" && pwd)"
+declare NVM_UNAME="$(command uname -a)"
+declare NVM_OS
+case "$NVM_UNAME" in
+Linux\ *)
+    NVM_OS=linux
+    ;;
+Darwin\ *)
+    NVM_OS=darwin
+    ;;
+SunOS\ *)
+    NVM_OS=sunos
+    ;;
+FreeBSD\ *)
+    NVM_OS=freebsd
+    ;;
+AIX\ *)
+    NVM_OS=aix
+    ;;
+esac
+
+declare MY_PATH
+if [[ "$NVM_OS" == "darwin" ]]; then
+    MY_PATH="$(cd "$(dirname "$0")" && pwd)"
+else
+    MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+fi
+
 source "$MY_PATH/my_sh.sh"
 source "$MY_PATH/my_net.sh"
 which fzf >/dev/null && source "$MY_PATH/my_fzf.sh"
